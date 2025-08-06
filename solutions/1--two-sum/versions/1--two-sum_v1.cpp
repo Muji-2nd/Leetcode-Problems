6bb0b2f825160a3ede6449807e519d79fec24dfe
Muji-2nd <f223403@cfd.nu.edu.pk>
2025-08-06 22:37:49 +0500

    Add version 1 of solution for 1. Two Sum

---
+++ solutions/1--two-sum/versions/1--two-sum_v1.cpp
@@ -0,0 +1,14 @@
+// LeetCode Problem: 1. Two Sum
+// Difficulty: Easy
+// Pushed on: 2025-08-06
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int>sum;
+        for(int i=0;i<nums.size();++i)
+        {
+             for(int j=0;j<nums.size();++j)
+        {
+            if(i!=j&&nums[i]+nums[j]==target)
+            {
