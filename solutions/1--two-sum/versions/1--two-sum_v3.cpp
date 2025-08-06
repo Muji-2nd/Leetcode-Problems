5c5d0fd541ac60fb65fce5ba441a5c6eeef9c551
Muji-2nd <f223403@cfd.nu.edu.pk>
2025-08-06 22:45:20 +0500

    Add version 3 of solution for 1. Two Sum

---
+++ solutions/1--two-sum/versions/1--two-sum_v3.cpp
@@ -0,0 +1,13 @@
+// LeetCode Problem: 1. Two Sum
+// Difficulty: Easy
+// Pushed on: 2025-08-06
+
+sum.push_back(i);
+                    sum.push_back(j);
+                return sum;
+            }
+        }
+        }
+        return sum;
+    }
+};
